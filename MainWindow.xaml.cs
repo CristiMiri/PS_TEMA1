ecf68b2e241cf2f0ae07ddad77bbcd5602b4b74a
Miritoiu Cristian <miritoiu.george@yahoo.com>
2024-03-30 14:10:24 +0200

    mainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using PS_TEMA1.Presenter;
+using PS_TEMA1.View.Interfaces;
+using PS_TEMA1.View.Pages;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +17,80 @@ namespace PS_TEMA1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMainGui
     {
+        private MainPresenter _mainPresenter;
         public MainWindow()
         {
             InitializeComponent();
+            //mainFrame.Content = new HomePage();
+            mainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            _mainPresenter = new MainPresenter(this);
+
+            mainFrame.Content = new AdminPage(_mainPresenter.ChangePage);
+            HideHeader();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Console.WriteLine("Hello World!");
+            //Repository repository = new Repository();
+            ////SELECT * FROM public."Tester";
+            ////print all the data from the table
+            //DataTable dataTable =repository.ExecuteQuery("SELECT * FROM public.\"Tester\";");
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    Console.WriteLine(row["id"]);
+            //}
+            //MessageBox.Show("Hello World!");
+
+            //main.Content = new LoginPage();
+
+            //UtilizatorDialog utilizatorDialog = new UtilizatorDialog();
+            //utilizatorDialog.ShowDialog();
+
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            _mainPresenter.ShowLoginPage();
+        }
+        private void Foms_Click(object sender, RoutedEventArgs e)
+        {
+            //UtilizatorDialog utilizatorDialog = new UtilizatorDialog();
+            //PrezentareDialog prezentareDialog = new PrezentareDialog();
+            //ConferintaDialog conferintaDialog = new ConferintaDialog();
+            //utilizatorDialog.ShowDialog();
+            //prezentareDialog.ShowDialog();
+            //conferintaDialog.ShowDialog();
+            mainFrame.Content = new HomePage();
+        }
+
+        private void main_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        public Frame GetFrame()
+        {
+            return mainFrame;
+        }
+
+        public void SetFrameContent(Page page)
+        {
+            mainFrame.Content = page;
+        }
+
+        public void ShowHeader()
+        {
+            Header.Visibility = Visibility.Visible;
+        }
+
+        public void HideHeader()
+        {
+            //loginButton.Visibility = Visibility.Collapsed;
+            Header.Visibility = Visibility.Collapsed;
         }
     }
 }
